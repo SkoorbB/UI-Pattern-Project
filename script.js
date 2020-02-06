9e75c233251c724be1afcda39a8cee29b9a60d99
Brooks <Skoorb412785@yahoo.com>
2020-02-06 15:57:27 -0500

    created for loop to make imgs in body

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+
+//found url
 let url = "https://bungie-net.github.io"
 fetch(url).then(res => {
-    console.log(':)', res)
+    //console logged to find position
+    console.log(':)', res);
     return res.json();
+})
+
+let body = document.querySelector(".grid");
+
+fetch(url).then(res => {
+    return res.json();
+})
+    .then(res => {
+    for (let i = 0; i < url.length; i++) {
+        if (url.length = 8) {
+            return;
+        }
+        let img = document.createElement("img");
+        img.classList.add("imgs");
+        body.appendChild(img);
+    }
 })
